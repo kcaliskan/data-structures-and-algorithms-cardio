6a02f6e0bd0881071d6f192e1e75ca829064b285
Kaan Caliskan <mail@kcaliskan.com>
2019-02-14 22:49:45 -0800

    added recursion factorial exercise

---
+++ algorithms/recursion/recursion-factorial.js
@@ -0,0 +1,11 @@
+// Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
+
+function findFactorialRecursive(number) {
+  //code here
+  return answer;
+}
+
+function findFactorialIterative(number) {
+  //code here
+  return answer;
+}
