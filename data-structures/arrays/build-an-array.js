b628a21a1e73199113ebedb830287647688f4fa1
Kaan Caliskan <mail@kcaliskan.com>
2019-05-04 13:11:01 -0700

    build an array from scratch

--- data-structures/arrays/build-an-array.js
+++ data-structures/arrays/build-an-array.js
@@ -1,10 +1,48 @@
-// class myArray{
-//   constructor(){
-//     this.myArray = {};
-//     this.length;
-//   }
-// }
+class MyArray {
+  constructor() {
+    this.myArray = {};
+    this.length = 0;
+  }
 
-let fruits = ['apple', 'banana', 'yes'];
+  push(item) {
+    this.myArray[this.length] = item;
+    this.length++;
+    return this.myArray;
+  }
 
-console.log(fruits)
+  get(index) {
+    return this.myArray[index];
+  }
+
+  pop() {
+    const lastItem = this.myArray[this.length - 1];
+
+    delete this.myArray[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const deletedItem = this.myArray[index];
+    return this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.myArray[i] = this.myArray[i + 1];
+    }
+    delete this.myArray[this.length - 1];
+    this.length--;
+    return this.myArray;
+  }
+}
+
+let newArray = new MyArray();
+console.log(newArray.push("kaan"));
+console.log(newArray.push("ipek"));
+console.log(newArray.push("elvis"));
+console.log(newArray.push("busi"));
+console.log(newArray.delete(2));
+
+// console.log(newArray.pop());
+// console.log(newArray.pop());
