6a33cc8ff92ae33fc55e0374e9126fe1c94c3485
Kaan Caliskan <mail@kcaliskan.com>
2019-05-09 23:32:19 -0700

    added playground js file

---
+++ data-structures/arrays/playground.js
@@ -0,0 +1,17 @@
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return null;
+};
+
+console.log(twoSum([2, 15, 11, 7], 9));
