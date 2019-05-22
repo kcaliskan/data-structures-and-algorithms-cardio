2fc964113cbbe9c7e19bdd3ef4902d297ffb0caa
Kaan Caliskan <mail@kcaliskan.com>
2019-05-22 14:25:39 -0700

    added the list of problems I solved

--- data-structures/hash-tables/index.js
+++ data-structures/hash-tables/index.js
@@ -1,6 +1,14 @@
 // Notes
-
 // 1-) Objects are type of hash tables in javascript.
+
+//Good
+// 1-) Fast lookups (Good collision resolution needed)
+// 2-) Fast Inserts
+// 3-) Flexible Keys
+
+//Bad
+// 1-) Unordered
+// 2-) Slow key iteration
 
 // Big O of Hash Tables
 // insert(1), lookup(1), delete(1), search(1)
---
+++ data-structures/solved.md
@@ -0,0 +1,6 @@
+## The Problems I Solved
+
+**Hash Tables**
+
+1. Jewels and Stones [Leetcode](https://leetcode.com/problems/jewels-and-stones/)
+2. Sort Array By Parity [Leetcode](https://leetcode.com/problems/sort-array-by-parity/)
